981a206b7cb46414cbcd2361a0f8fb213a5e0c5a
Barry Xu <barry.xu@sony.com>
2024-04-24 17:05:35 +0800

    Add new configuring interfaces to enable service intropsection for action
    Signed-off-by: Barry Xu <barry.xu@sony.com>

--- rclpy/rclpy/action/server.py
+++ rclpy/rclpy/action/server.py
@@ -19,11 +19,13 @@ import traceback
 
 from action_msgs.msg import GoalInfo, GoalStatus
 
+from rclpy.clock import Clock
 from rclpy.executors import await_or_execute
 from rclpy.impl.implementation_singleton import rclpy_implementation as _rclpy
 from rclpy.qos import qos_profile_action_status_default
 from rclpy.qos import qos_profile_services_default
 from rclpy.qos import QoSProfile
+from rclpy.service_introspection import ServiceIntrospectionState
 from rclpy.task import Future
 from rclpy.type_support import check_for_type_support
 from rclpy.waitable import NumberOfEntities, Waitable
@@ -615,6 +617,23 @@ class ActionServer(Waitable):
         if not callable(execute_callback):
             raise TypeError('Failed to register goal execution callback: not callable')
         self._execute_callback = execute_callback
+
+    def configure_introspection(
+        self, clock: Clock,
+        service_event_qos_profile: QoSProfile,
+        introspection_state: ServiceIntrospectionState
+    ) -> None:
+        """
+        Configure internal service introspection.
+
+        :param clock: Clock to use for generating timestamps.
+        :param service_event_qos_profile: QoSProfile to use when creating service event publisher.
+        :param introspection_state: ServiceIntrospectionState to set introspection.
+        """
+        with self._handle:
+            self._handle.configure_introspection(clock.handle,
+                                                   service_event_qos_profile.get_c_qos_profile(),
+                                                   introspection_state)
 
     def destroy(self):
         """Destroy the underlying action server handle."""
